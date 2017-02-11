ccb43d8d8ede67ce5db6d2ab5bfd78fd3dbf3891
Furkan Sergen Dayıoğlu <fdayioglu@gtu.edu.tr>
2017-02-11 18:00:37 +0200

    Add files via upload

---
+++ blog_ders 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+/*include ifadeleri bir kutuphane eklemek icindir */
+/* Temel bir C kodu, bir include ifadesi, main fonksiyonu , return ifadesinden
+(cogu derleyici main fonksiyonu icin bir donus tipi ve bir donus degeri ister)
+olusur */
+
+int main(void){
+	
+	printf("Merhaba Dunya!!!\n");
+	
+	return 0;
+}
